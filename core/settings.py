4bf60a876c7bbde4eb3cf84a98332874fc669141
arian radmehr <arian.radmehr.bw@gmail.com>
2021-08-08 18:56:30 +0430

    made some changes

--- core/settings.py
+++ core/settings.py
@@ -143,4 +143,4 @@ EMAIL_PORT = 587
 # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_USER = 'arian.radmehr.bw@gmail.com'
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_PASSWORD = 'togleps34jalinoos'
+EMAIL_HOST_PASSWORD = 'enter the password for your email'
