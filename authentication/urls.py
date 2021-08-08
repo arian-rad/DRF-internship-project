76ad5a94d158572e21cd04e63ae3860633cf70ca
arian radmehr <arian.radmehr.bw@gmail.com>
2021-08-08 15:31:55 +0430

    video 13, Refresh token and Access token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .api_views import RegisterAPIView, VerifyEmailAPIView, LoginAPIView, UserRetrieveDestroyAPIView, UserListAPIView
 
 
@@ -8,6 +9,7 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('verify-email/', VerifyEmailAPIView.as_view(), name='email_verify'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('all-users/', UserListAPIView.as_view(), name='all_users'),
     path('delete-or-retrieve-user/<int:pk>/', UserRetrieveDestroyAPIView.as_view(), name='delete_user'),
 
